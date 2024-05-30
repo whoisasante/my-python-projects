eddbfe994934e08f8a419c54f73414fe3cc96a4f
Richard Kwabena Osei-Asante <oseiasantesmith123@gmail.com>
2024-05-30 12:44:07 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome. This is a football team name generator.")
+city = input("What city did you grow up in?\n")
+pet = input("What is the name of your pet?\n")
+print("Your band name is " + city + " " + pet)
